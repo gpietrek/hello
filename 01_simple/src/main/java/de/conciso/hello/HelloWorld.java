18a9dda4b24523194d29dbb550b839841ae55ce1
Georg Pietrek <georg.pietrek@conciso.de>
2023-04-05 17:30:34 +0200

    add hello world output

--- 01_simple/src/main/java/de/conciso/hello/HelloWorld.java
+++ 01_simple/src/main/java/de/conciso/hello/HelloWorld.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloWorld {
   public static void main(String[] args) {
+    System.out.println("Hello world!");
     SpringApplication.run(HelloWorld.class);
   }
 }
