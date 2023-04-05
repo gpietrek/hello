1d6ddc5f667e5946134aa7ef6f7e600be2440a10
Georg Pietrek <georg.pietrek@conciso.de>
2023-04-05 17:43:45 +0200

    remove hello world output

--- 01_simple/src/main/java/de/conciso/hello/HelloWorld.java
+++ 01_simple/src/main/java/de/conciso/hello/HelloWorld.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
     SpringApplication.run(HelloWorld.class);
   }
 }
